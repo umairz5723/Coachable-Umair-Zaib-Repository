8cfa822d61f1d08dca652c266f28209f2e8c274f
umairz5723 <108788149+umairz5723@users.noreply.github.com>
2025-01-09 17:36:28 -0500

    Update 5_longest_palindromic_substring.py

--- leetcode/5_longest_palindromic_substring.py
+++ leetcode/5_longest_palindromic_substring.py
@@ -4,34 +4,35 @@ from typing import List
 
 class Solution:
         """ Solution Class """
-    def longestPalindrome(self, s: str) -> str:
-        """
-        
-        This function makes use of a helper method "expand" to expand left and right pointers
-        and return palindrome.
-        We collect palindromes of odd and even length on the main loop and replace the existing
-        longest plaindrome seen thus far if needed.
+        def longestPalindrome(self, s: str) -> str:
+            """
+            
+            This function makes use of a helper method "expand" 
+            to expand left and right pointers and return palindrome.
+            We collect palindromes of odd and even length on the main loop 
+            and replace the existinglongest plaindrome seen thus far 
+            if needed.
 
-        """
-        if len(s) == 1:
-            return s[0]
-        
-        def expand(l,r):
+            """
+            if len(s) == 1:
+                return s[0]
+            
+            def expand(l,r):
 
-            while l >= 0 and r < len(s) and s[l] == s[r]:
-                l-=1
-                r+=1
-            return s[l+1:r]
+                while l >= 0 and r < len(s) and s[l] == s[r]:
+                    l-=1
+                    r+=1
+                return s[l+1:r]
 
-        res = [s[0]]
+            res = s[0]
 
-        for i in range(len(s)-1):
-            odd = expand(i,i)
-            even = expand(i, i+1)
+            for i in range(len(s)-1):
+                odd = expand(i,i)
+                even = expand(i, i+1)
 
-            if len(odd) > len(res[0]):
-                res[0] = odd
-            if len(even) > len(res[0]):
-                res[0] = even
-        
-        return res[0]
+                if len(odd) > len(res):
+                    res = odd
+                if len(even) > len(res):
+                    res = even
+            
+            return res
