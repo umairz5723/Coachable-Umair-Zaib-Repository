7be70464714eccec01d60eb9aa6b47632ddcc8ba
umairz5723 <108788149+umairz5723@users.noreply.github.com>
2025-01-12 00:04:01 -0500

    Update 160_Intersection_of_Two_Linked Lists.py

--- leetcode/160_Intersection_of_Two_Linked Lists.py
+++ leetcode/160_Intersection_of_Two_Linked Lists.py
@@ -1,14 +1,33 @@
-"""160. Intersection of Two Lists """
+"""160. Intersection of Two Lists"""
+from typing import Optional
+
+# pylint: disable=too-few-public-methods
+class ListNode:
+    """Definition for singly-linked list node"""
+    def __init__(self, val=0, next_node=None):  # Changed 'next' to 'next_node'
+        self.val = val
+        self.next = next_node
+
 class Solution:
-    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
+    """Solution class for finding intersection of two linked lists"""
+
+    def get_intersection_node(self, head_a: ListNode, head_b: ListNode) -> Optional[ListNode]:
+        """
+        Find the intersection node of two linked lists using the two pointer technique.
         
-        
-        p1 = headA
-        p2 = headB
+        Args:
+            head_a: Head of first linked list
+            head_b: Head of second linked list
+            
+        Returns:
+            ListNode: Intersection node if exists, else None
+        """
+        p1 = head_a
+        p2 = head_b
 
         while p1 != p2:
+            p1 = p1.next if p1 else head_a
+            p2 = p2.next if p2 else head_b
 
-            p1 = p1.next if p1 else headB
-            p2 = p2.next if p2 else headA
-        
         return p1
+    
