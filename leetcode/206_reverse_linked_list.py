e805e4665aa952485476cc82f8c77aac830c0e0c
umairz5723 <108788149+umairz5723@users.noreply.github.com>
2025-01-12 00:05:58 -0500

    Update 206_reverse_linked_list.py

--- leetcode/206_reverse_linked_list.py
+++ leetcode/206_reverse_linked_list.py
@@ -1,18 +1,35 @@
-"""206. Reverse Linked List """
+"""206. Reverse Linked List"""
+from typing import Optional
 
+class ListNode:
+    """Definition for singly-linked list node"""
+    def __init__(self, val=0, next_node=None):
+        self.val = val
+        self.next = next_node
+
+# pylint: disable=too-few-public-methods
 class Solution:
-    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+    """Solution class for reversing a linked list"""
+
+    def reverse_list(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        """
+        Reverses a linked list iteratively.
         
+        Args:
+            head: Head of the linked list to reverse
+            
+        Returns:
+            ListNode: Head of the reversed linked list
+        """
         if not head:
             return None
-        
+
         cur = head
         prev = None
-
         while cur:
             next_node = cur.next
             cur.next = prev
             prev = cur
             cur = next_node
-
         return prev
+    
