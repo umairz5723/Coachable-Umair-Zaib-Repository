c888a4522e0136254e4b33c27957b7be79c48447
umairz5723 <108788149+umairz5723@users.noreply.github.com>
2025-02-07 22:45:27 -0500

    Update and rename 211_design_add_and_search_words_data_structure.py to design_add_and_search_words_data_structure.py

--- leetcode/design_add_and_search_words_data_structure.py
+++ leetcode/design_add_and_search_words_data_structure.py
@@ -1,27 +1,32 @@
 """ 211. Design Add and Search Words Data Structure """
 
 class TrieNode():
-    
+    """ TrieNode Class """
     def __init__(self):
         self.children = {}
-        self.isWord = False
+        self.is_word = False
 
 class WordDictionary:
-  
+
     """
     
-    This solution uses the exact same setup from first Trie problem, in that we create TrieNodes of characters
-    at each level when needed and mark the final character as a word. The only difference in the search method is the 
-    handling of the the "." character in which we must use a dfs function that keeps track of an idx and node. When we match 
-    with a "." we check if the next index and child lead to a word and return True/False based on this. Otherwise, we handle
+    This solution uses the exact same setup from first Trie problem, 
+    in that we create TrieNodes of characters
+    at each level when needed and mark the final character as a word. 
+    The only difference in the search method is the 
+    handling of the the "." character in which we must use a dfs function 
+    that keeps track of an idx and node. When we match 
+    with a "." we check if the next index and child lead to a word and 
+    return True/False based on this. Otherwise, we handle
     alphabetical characters the exact same in the first problem.
 
     """
     def __init__(self):
         self.root = TrieNode()
 
-    def addWord(self, word: str) -> None:
-        
+    def add_word(self, word: str) -> None:
+        """ Function to add word into Trie """
+
         cur = self.root
 
         for ch in word:
@@ -29,10 +34,11 @@ class WordDictionary:
                 cur.children[ch] = TrieNode()
             cur = cur.children[ch]
 
-        cur.isWord = True
+        cur.is_word = True
 
 
     def search(self, word: str) -> bool:
+        """ Function to search for a word in Trie """
 
         def dfs(idx, node):
             cur = node
@@ -48,7 +54,7 @@ class WordDictionary:
                     if ch not in cur.children:
                         return False
                     cur = cur.children[ch]
-            
-            return cur.isWord
-        
+
+            return cur.is_word
+
         return dfs(0, self.root)
